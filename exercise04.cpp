ce595c4fc07867bb99f93e53ac58c5de7276e70e
Tharindu Lakshan <96631498+CoorayNTL@users.noreply.github.com>
2022-02-21 18:50:35 +0000

    Finished

--- exercise04.cpp
+++ exercise04.cpp
@@ -13,9 +13,16 @@ int main() {
 }
 
 // Do not change the print() function
-void print(int len, int wth) {
+  void print(int len, int wth) {
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
 }
 
 // Implement the Input Function here
+void input(int& len, int& wth){
+  
+int temp=len;
+len = wth;
+wth = temp;
+  
+}
